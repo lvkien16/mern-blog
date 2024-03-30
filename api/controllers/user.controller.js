92d6487c7221935f4dd59b415f1ae778120ca095
kienprovip <lovankien@gmail.com>
2024-03-30 16:08:06 +0700

    add signout functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -7,62 +7,70 @@ export const test = (req, res) => {
 }
 
 export const updateUser = async (req, res, next) => {
-    if (req.user.id !== req.params.userId) {
-      return next(errorHandler(403, 'You are not allowed to update this user'));
+  if (req.user.id !== req.params.userId) {
+    return next(errorHandler(403, 'You are not allowed to update this user'));
+  }
+  if (req.body.password) {
+    if (req.body.password.length < 6) {
+      return next(errorHandler(400, 'Password must be at least 6 characters'));
     }
-    if (req.body.password) {
-      if (req.body.password.length < 6) {
-        return next(errorHandler(400, 'Password must be at least 6 characters'));
-      }
-      req.body.password = bcryptjs.hashSync(req.body.password, 10);
-    }
-    if (req.body.username) {
-      if (req.body.username.length < 7 || req.body.username.length > 20) {
-        return next(
-          errorHandler(400, 'Username must be between 7 and 20 characters')
-        );
-      }
-      if (req.body.username.includes(' ')) {
-        return next(errorHandler(400, 'Username cannot contain spaces'));
-      }
-      if (req.body.username !== req.body.username.toLowerCase()) {
-        return next(errorHandler(400, 'Username must be lowercase'));
-      }
-      if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
-        return next(
-          errorHandler(400, 'Username can only contain letters and numbers')
-        );
-      }
-    }
-    try {
-      const updatedUser = await User.findByIdAndUpdate(
-        req.params.userId,
-        {
-          $set: {
-            username: req.body.username,
-            email: req.body.email,
-            profilePicture: req.body.profilePicture,
-            password: req.body.password,
-          },
-        },
-        { new: true }
+    req.body.password = bcryptjs.hashSync(req.body.password, 10);
+  }
+  if (req.body.username) {
+    if (req.body.username.length < 7 || req.body.username.length > 20) {
+      return next(
+        errorHandler(400, 'Username must be between 7 and 20 characters')
       );
-      const { password, ...rest } = updatedUser._doc;
-      res.status(200).json(rest);
-    } catch (error) {
-      next(error);
     }
-  };
-
-  export const deleteUser = async (req, res, next) => {
-    if(req.user.id !== req.params.userId){
-      return next(errorHandler(403, 'You are not allowed to delete this user'));
+    if (req.body.username.includes(' ')) {
+      return next(errorHandler(400, 'Username cannot contain spaces'));
     }
-
-    try {
-        await User.findByIdAndDelete(req.params.userId);
-        res.status(200).json({message: 'User deleted successfully'});
-    } catch (error) {
-      next(error);
+    if (req.body.username !== req.body.username.toLowerCase()) {
+      return next(errorHandler(400, 'Username must be lowercase'));
+    }
+    if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
+      return next(
+        errorHandler(400, 'Username can only contain letters and numbers')
+      );
     }
   }
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.userId,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          profilePicture: req.body.profilePicture,
+          password: req.body.password,
+        },
+      },
+      { new: true }
+    );
+    const { password, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+if(req.user.id !== req.params.userId){
+  return next(errorHandler(403, 'You are not allowed to delete this user'));
+}
+
+try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json({message: 'User deleted successfully'});
+} catch (error) {
+  next(error);
+}
+};
+
+export const signout = (req, res, next) => {
+  try {
+    res.clearCookie('access_token').status(200).json({ message: 'You has been signed out' });
+  } catch (error) {
+    next(error);
+  }
+};
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -49,6 +49,11 @@ const userSlice = createSlice({
             state.loading = false;
             state.error = action.payload;
         },
+        signoutSuccess: (state) => {
+            state.currentUser = null;
+            state.error = null;
+            state.loading = false;
+        },
     },
 });
 
@@ -61,7 +66,8 @@ export const {
     updateFailure, 
     deleteUserStart,
     deleteUserSuccess,
-    deleteUserFailure
+    deleteUserFailure,
+    signoutSuccess
     } = userSlice.actions;
 
 export default userSlice.reducer;
