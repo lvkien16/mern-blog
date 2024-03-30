01809d5618039bff700aec7ec65797cfb3a3c81e
kienprovip <lovankien@gmail.com>
2024-03-30 15:01:10 +0700

    complete update user profile page functionality

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -23,9 +23,22 @@ const userSlice = createSlice({
             state.loading = false;
             state.error = action.payload;
         },
+        updateStart: (state) => {
+            state.loading = true;
+            state.error = null;
+        },
+        updateSuccess: (state, action) => {
+            state.currentUser = action.payload;
+            state.loading = false;
+            state.error = null;
+        },
+        updateFailure: (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
+        },
     },
 });
 
-export const { signInStart, signInSuccess, singInFailure} = userSlice.actions;
+export const { signInStart, signInSuccess, singInFailure, updateStart, updateSuccess, updateFailure} = userSlice.actions;
 
 export default userSlice.reducer;
