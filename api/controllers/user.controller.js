e7af8296a391ef0b2c2eee80423af7d10f158990
kienprovip <lovankien@gmail.com>
2024-03-30 15:09:51 +0700

    add delete user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -53,3 +53,16 @@ export const updateUser = async (req, res, next) => {
       next(error);
     }
   };
+
+  export const deleteUser = async (req, res, next) => {
+    if(req.user.id !== req.params.userId){
+      return next(errorHandler(403, 'You are not allowed to delete this user'));
+    }
+
+    try {
+        await User.findByIdAndDelete(req.params.userId);
+        res.status(200).json({message: 'User deleted successfully'});
+    } catch (error) {
+      next(error);
+    }
+  }
