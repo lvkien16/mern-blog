0c09c2a76a429765dfdf56a795ae59ee13fcc067
kienprovip <lovankien@gmail.com>
2024-04-01 21:47:03 +0700

    add create a post api route

---
+++ api/controllers/post.controller.js
@@ -0,0 +1,21 @@
+import Post from "../models/post.model.js";
+import { errorHandler } from "../utils/error.js"
+
+export const create = async (req, res, next) => {
+    if(!req.user.isAdmin){
+        return next(errorHandler(403, "You are not allowed to create a post"));
+    }
+    if(!req.body.title || !req.body.content){
+        return next(errorHandler(400, "Please provide all the required fields"));
+    }
+    const slug = req.body.title.split(' ').join('-').toLowerCase().replace(/[^a-zA-Z0-9-]/g, '-');
+    const newPost = new Post( {
+        ...req.body, slug, userId: req.user.id,
+    });
+    try {
+        const savePost = await newPost.save();
+        res.status(201).json(savePost);
+    } catch (error) {
+        next(error);
+    }
+};
