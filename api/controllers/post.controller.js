71ac70f2d50e2c6cdc7d2c38b219301bff6a21ca
kienprovip <lovankien@gmail.com>
2024-04-03 09:02:39 +0700

    complete create post functionality

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -8,13 +8,13 @@ export const create = async (req, res, next) => {
     if(!req.body.title || !req.body.content){
         return next(errorHandler(400, "Please provide all the required fields"));
     }
-    const slug = req.body.title.split(' ').join('-').toLowerCase().replace(/[^a-zA-Z0-9-]/g, '-');
+    const slug = req.body.title.split(' ').join('-').toLowerCase().replace(/[^a-zA-Z0-9-]/g, '');
     const newPost = new Post( {
         ...req.body, slug, userId: req.user.id,
     });
     try {
-        const savePost = await newPost.save();
-        res.status(201).json(savePost);
+        const savedPost = await newPost.save();
+        res.status(201).json(savedPost);
     } catch (error) {
         next(error);
     }
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -11,22 +11,27 @@ import { app } from "../firebase";
 import { useState } from "react";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
+import { useNavigate } from "react-router-dom";
 
 export default function CreatePost() {
   const [file, setFile] = useState(null);
   const [imageUploadProgress, setImageUploadProgress] = useState(null);
   const [imageUploadError, setImageUploadError] = useState(null);
   const [formData, setFormData] = useState({});
-  const handleUploadImage = async () => {
+  const [publishError, setPublishError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleUpdloadImage = async () => {
     try {
       if (!file) {
-        setImageUploadError("Please select an image to upload");
+        setImageUploadError("Please select an image");
         return;
       }
       setImageUploadError(null);
       const storage = getStorage(app);
-      const filezName = new Date().getTime() + "-" + file.name;
-      const storageRef = ref(storage, filezName);
+      const fileName = new Date().getTime() + "-" + file.name;
+      const storageRef = ref(storage, fileName);
       const uploadTask = uploadBytesResumable(storageRef, file);
       uploadTask.on(
         "state_changed",
@@ -36,7 +41,7 @@ export default function CreatePost() {
           setImageUploadProgress(progress.toFixed(0));
         },
         (error) => {
-          setImageUploadError("Image upload failed. Please try again later");
+          setImageUploadError("Image upload failed");
           setImageUploadProgress(null);
         },
         () => {
@@ -48,27 +53,57 @@ export default function CreatePost() {
         }
       );
     } catch (error) {
-      setImageUploadError("Image upload failed. Please try again later");
+      setImageUploadError("Image upload failed");
       setImageUploadProgress(null);
       console.log(error);
+    }
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch("/api/post/create", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        setPublishError(data.message);
+        return;
+      }
+
+      if (res.ok) {
+        setPublishError(null);
+        navigate(`/post/${data.slug}`);
+      }
+    } catch (error) {
+      setPublishError("Something went wrong");
     }
   };
   return (
     <div className="p-3 max-w-3xl mx-auto min-h-screen">
       <h1 className="text-center text-3xl my-7 font-semibold">Create a post</h1>
-      <form className="flex flex-col gap-4">
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
         <div className="flex flex-col gap-4 sm:flex-row justify-between">
           <TextInput
             type="text"
             placeholder="Title"
-            id="title"
-            name="title"
-            className="flex-1"
             required
+            id="title"
+            className="flex-1"
+            onChange={(e) =>
+              setFormData({ ...formData, title: e.target.value })
+            }
           />
-          <Select>
+          <Select
+            onChange={(e) =>
+              setFormData({ ...formData, category: e.target.value })
+            }
+          >
             <option value="uncategorized">Select a category</option>
-            <option value="javascript">Javascript</option>
+            <option value="javascript">JavaScript</option>
             <option value="reactjs">React.js</option>
             <option value="nextjs">Next.js</option>
           </Select>
@@ -84,7 +119,7 @@ export default function CreatePost() {
             gradientDuoTone="purpleToBlue"
             size="sm"
             outline
-            onClick={handleUploadImage}
+            onClick={handleUpdloadImage}
             disabled={imageUploadProgress}
           >
             {imageUploadProgress ? (
@@ -112,10 +147,18 @@ export default function CreatePost() {
           placeholder="Write something..."
           className="h-72 mb-12"
           required
+          onChange={(value) => {
+            setFormData({ ...formData, content: value });
+          }}
         />
         <Button type="submit" gradientDuoTone="purpleToPink">
           Publish
         </Button>
+        {publishError && (
+          <Alert className="mt-5" color="failure">
+            {publishError}
+          </Alert>
+        )}
       </form>
     </div>
   );
