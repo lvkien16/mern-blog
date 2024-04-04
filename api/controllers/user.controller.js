16b3aded27e0a5a67b00e5af64d121e3b8d445f2
kienprovip <lovankien@gmail.com>
2024-04-04 21:57:24 +0700

    create get users api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,36 +1,36 @@
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
-import User from '../models/user.model.js';
+import User from "../models/user.model.js";
 
 export const test = (req, res) => {
-    res.json({message: "API is working"});
-}
+  res.json({ message: "API is working" });
+};
 
 export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.userId) {
-    return next(errorHandler(403, 'You are not allowed to update this user'));
+    return next(errorHandler(403, "You are not allowed to update this user"));
   }
   if (req.body.password) {
     if (req.body.password.length < 6) {
-      return next(errorHandler(400, 'Password must be at least 6 characters'));
+      return next(errorHandler(400, "Password must be at least 6 characters"));
     }
     req.body.password = bcryptjs.hashSync(req.body.password, 10);
   }
   if (req.body.username) {
     if (req.body.username.length < 7 || req.body.username.length > 20) {
       return next(
-        errorHandler(400, 'Username must be between 7 and 20 characters')
+        errorHandler(400, "Username must be between 7 and 20 characters")
       );
     }
-    if (req.body.username.includes(' ')) {
-      return next(errorHandler(400, 'Username cannot contain spaces'));
+    if (req.body.username.includes(" ")) {
+      return next(errorHandler(400, "Username cannot contain spaces"));
     }
     if (req.body.username !== req.body.username.toLowerCase()) {
-      return next(errorHandler(400, 'Username must be lowercase'));
+      return next(errorHandler(400, "Username must be lowercase"));
     }
     if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
       return next(
-        errorHandler(400, 'Username can only contain letters and numbers')
+        errorHandler(400, "Username can only contain letters and numbers")
       );
     }
   }
@@ -55,21 +55,66 @@ export const updateUser = async (req, res, next) => {
 };
 
 export const deleteUser = async (req, res, next) => {
-if(req.user.id !== req.params.userId){
-  return next(errorHandler(403, 'You are not allowed to delete this user'));
-}
+  if (req.user.id !== req.params.userId) {
+    return next(errorHandler(403, "You are not allowed to delete this user"));
+  }
 
-try {
+  try {
     await User.findByIdAndDelete(req.params.userId);
-    res.status(200).json({message: 'User deleted successfully'});
-} catch (error) {
-  next(error);
-}
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const signout = (req, res, next) => {
   try {
-    res.clearCookie('access_token').status(200).json({ message: 'You has been signed out' });
+    res
+      .clearCookie("access_token")
+      .status(200)
+      .json({ message: "You has been signed out" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUsers = async (req, res, next) => {
+  if (!req.user.isAdmin) {
+    return next(errorHandler(403, "You are not allowed to see all users"));
+  }
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.sort === "asc" ? 1 : -1;
+
+    const users = await User.find()
+      .sort({ createdAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const usersWithoutPassword = users.map((user) => {
+      const { password, ...rest } = user._doc;
+      return rest;
+    });
+
+    const totalUsers = await User.countDocuments();
+
+    const now = new Date();
+
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+    const lastMonthUsers = await User.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    res.status(200).json({
+      users: usersWithoutPassword,
+      totalUsers,
+      lastMonthUsers,
+    });
   } catch (error) {
     next(error);
   }
