0be247ed55dc9a104fa2da5033c899e9ebf023e1
kienprovip <lovankien@gmail.com>
2024-04-07 17:50:29 +0700

    show the comments of a post

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -1,24 +1,30 @@
 import Comment from "../models/comment.model.js";
-import { errorHandler } from "./../utils/error.js";
-
 export const createComment = async (req, res, next) => {
   try {
     const { content, postId, userId } = req.body;
-
     if (userId !== req.user.id) {
       return next(
         errorHandler(403, "You are not allowed to create this comment")
       );
     }
-
     const newComment = new Comment({
       content,
       postId,
       userId,
     });
     await newComment.save();
-
     res.status(200).json(newComment);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getPostComments = async (req, res, next) => {
+  try {
+    const comments = await Comment.find({ postId: req.params.postId }).sort({
+      createdAt: -1,
+    });
+    res.status(200).json(comments);
   } catch (error) {
     next(error);
   }
