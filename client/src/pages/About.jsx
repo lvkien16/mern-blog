34ace879d39046959d5a8d8393c45da21c892038
kienprovip <lovankien@gmail.com>
2024-04-12 21:35:12 +0700

    complete about and projects pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,33 @@
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font-semibold text-center my-7">
+            About Kien's Blog
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque,
+              dolores ipsum! Doloremque blanditiis laborum repudiandae porro
+              vitae error accusamus adipisci suscipit corporis quidem, animi
+              deserunt aut iure eaque voluptatum! Ipsa?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo
+              labore magni optio nesciunt, accusantium, error eius id eveniet ad
+              vero est odit dolorum sunt? Exercitationem saepe nihil ut.
+              Veritatis, sequi!
+            </p>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+              veniam modi distinctio dolorem similique quod exercitationem earum
+              debitis consectetur dicta sunt accusantium magni vero dolores,
+              quibusdam consequuntur labore! Repellat, nihil?
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -145,7 +145,7 @@ export default function Search() {
         <h1 className="text-3xl font-semibold sm:border-b border-gray-500 p-3 mt-5 ">
           Posts results:
         </h1>
-        <div className="p-7 flex flex-wrap gap-4">
+        <div className="p-7 flex flex-wrap gap-4 justify-center">
           {!loading && posts.length === 0 && (
             <p className="text-xl text-gray-500">No posts found.</p>
           )}
